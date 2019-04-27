6065cdddeeea4702de2b35719f9798a7e8ef1125
qiaolibo <qiaolibo@wafersystems.com>
2019-04-27 10:39:31 +0800

    Add openjdk 8; Oracle JRE 8

--- update.sh
+++ update.sh
@@ -2,5 +2,15 @@
 set -Eeuo pipefail
 
 generate-dockerfile() {
-  local package="$1"; shift
+  local java_vendor="$1"; shift
+  local java_version="$1"; shift
+
+  mkdir -p $java_vendor/$java_version
+ 
+  sed "1s#java_vendor#$java_vendor#" Dockerfile.template > $java_vendor/$java_version/Dockerfile
+  sed "1s#java_version#$java_version#" Dockerfile.template > $java_vendor/$java_version/Dockerfile
 }
+
+# Generate Dockerfile
+generate-dockerfile openjdk 8-jre-alpine
+generate-dockerfile frolvlad/alpine-oraclejre8 latest
